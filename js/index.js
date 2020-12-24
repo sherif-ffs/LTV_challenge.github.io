e2b78f8980440b9dcb5cc898f4f645211775a551
Sherif Elmetwally <sge5@njit.edu>
2020-12-24 16:54:31 -0500

    render result on search

--- js/index.js
+++ js/index.js
@@ -4,7 +4,17 @@ const SearchEmail = (function(){
         searchBar: document.querySelector('.email-searchbar'),
         submitButton: document.querySelector('.submit-button'),
         errorFlag: document.querySelector('.error-flag'),
-        resultsSection: document.querySelector('.results-section')
+        resultsSection: document.querySelector('.results-section'),
+        addressDetails: document.querySelector('.address-details'),
+        emailDetails: document.querySelector('.email-details'),
+        phoneHeader: document.querySelector('.phone-header'),
+        relativesHeader: document.querySelector('.relatives-header'),
+        nameAndAge: document.querySelector('.name-age'),
+        userDetails: document.querySelector('.user-details'),
+        resultsSection: document.querySelector('.results-section'),
+        resultsSectionHeader: document.querySelector('.results-section-header'),
+        resultsSectionSubHeader: document.querySelector('.results-section-sub-header'),
+        resultWrapper: document.querySelector('.result')
     }
 
     state = {
@@ -40,6 +50,50 @@ const SearchEmail = (function(){
         }
     }
 
+    const renderNoResultsFound = () => {
+        elements.resultWrapper.style.display = 'none';
+        elements.resultsSectionHeader.textContent = '0 Results'
+        elements.resultsSectionSubHeader.textContent = 'Try starting a new search below';
+        elements.resultsSection.style.display = 'flex'
+    }
+
+    const populateResultWrapper = (searchResult) => {
+        console.log('searchResult: ', searchResult)
+        elements.resultWrapper.style.display = 'flex';
+        const name = searchResult.first_name + ' ' + searchResult.last_name
+        const description = searchResult.description
+        const email = searchResult.email
+        const address = searchResult.address
+        const phoneNumbers = searchResult.phone_numbers
+        const relatives = searchResult.relatives
+
+        elements.nameAndAge.textContent = name;
+        elements.userDetails.textContent = description
+        elements.addressDetails.textContent = address
+        elements.emailDetails.textContent = email
+
+        // remove existing phone numbers before creating and appending new ones
+        document.querySelectorAll('.phone-details').forEach(e => e.remove())
+
+        phoneNumbers.forEach(number => {
+            let numberElement = document.createElement('p')
+            numberElement.classList.add('phone-details')
+            numberElement.textContent = `(${number.substring(0,3)}) ${number.substring(3,6)}-${number.substring(6,9)}`
+            elements.phoneHeader.appendChild(numberElement)
+        })
+
+        // remove existing relatives before creating and appending new ones
+        document.querySelectorAll('.relatives-details').forEach(e => e.remove())
+        relatives.forEach(relative => {
+            let relativeElement = document.createElement('p')
+            relativeElement.classList.add('phone-details')
+            relativeElement.textContent = relative
+            elements.relativesHeader.appendChild(relativeElement)
+        })
+
+        elements.resultsSection.style.display = 'flex'
+    }
+
     const submitSearch = async () => {
         const value = elements.searchBar.value;
         if (!validateEmail(value)) {
@@ -49,9 +103,17 @@ const SearchEmail = (function(){
         } else {
             removeErrorMessage()
             document.querySelector('.reverse-email-lookup').style.display = 'none';
-            elements.resultsSection.style.display = 'flex'
         }
         const searchResult = await callApi(value);
+        console.log('searchResult: ', searchResult)
+        if (searchResult != []) {
+            console.log('if hits')
+            populateResultWrapper(searchResult)
+        } else {
+            console.log('else hits')
+            renderNoResultsFound()
+        }
+        // populateResultWrapper(searchResult)
         state.searchResults = searchResult;
     }
 
