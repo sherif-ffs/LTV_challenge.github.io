38390b2970e8b3fc1efe3096660bb352288d4d5a
Sherif Elmetwally <sge5@njit.edu>
2020-12-24 00:46:04 -0500

    render error message and results section

--- js/index.js
+++ js/index.js
@@ -2,22 +2,57 @@ const SearchEmail = (function(){
 
     elements = {
         searchBar: document.querySelector('.email-searchbar'),
-        submitButton: document.querySelector('.submit-button')
+        submitButton: document.querySelector('.submit-button'),
+        errorFlag: document.querySelector('.error-flag'),
+        resultsSection: document.querySelector('.results-section')
     }
 
-    const callApi = (value) => {
+    state = {
+        searchResults: []
+    }
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const renderErrorMessage = () => {
+        elements.errorFlag.style.visibility = 'visible';
+        elements.searchBar.style.border = '3px solid #DC0015';
+    }
+
+    const removeErrorMessage = () => {
+        elements.errorFlag.style.visibility = 'hidden';
+        elements.searchBar.style.border = 'none';
+    }
+
+    const callApi = async (value) => {
         const proxyurl = "https://cors-anywhere.herokuapp.com/"; // use proxy to get around CORS issue
         const url = `https://ltv-data-api.herokuapp.com/api/v1/records.json?email=${value}`;
-        fetch(proxyurl + url) 
-        .then(response => response.json())
-        .then(contents => console.log(contents))
-        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+        try {
+            const response = await fetch(proxyurl + url)
+            const result = await response.json();
+            console.log('result: ', result)
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    const submitSearch = () => {
+    const submitSearch = async () => {
         const value = elements.searchBar.value;
-        callApi(value)
-        console.log('value: ', value)
+        if (!validateEmail(value)) {
+            console.log('email is not valid')
+            renderErrorMessage()
+            return
+        } else {
+            removeErrorMessage()
+            document.querySelector('.reverse-email-lookup').style.display = 'none';
+            elements.resultsSection.style.display = 'flex'
+        }
+        const searchResult = await callApi(value);
+        state.searchResults = searchResult;
     }
 
     const init = () => {
